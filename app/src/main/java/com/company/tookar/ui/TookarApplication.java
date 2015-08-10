1574adcdd16d6ff9375e010f2a615acba451ef66
Amirul Zin <amirul.mdzin@gmail.com>
2015-08-10 22:20:48 +0800

    Initial commit

---
+++ app/src/main/java/com/company/tookar/ui/TookarApplication.java
@@ -0,0 +1,12 @@
+package com.company.tookar.ui;
+
+import android.app.Application;
+
+public class TookarApplication extends Application
+{
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/com/company/tookar/ui/activity/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.company.tookar.ui.activity;
+
+import android.os.Bundle;
+
+import com.company.tookar.R;
+
+public class LoginActivity extends BaseActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
