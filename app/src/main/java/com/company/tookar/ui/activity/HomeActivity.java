48a1f2abba73d14cfdad5ea17357dab0849706dd
Amirul Zin <amirul.mdzin@gmail.com>
2015-08-11 00:54:12 +0800

    Resources import. Prototype for Home, Login, and Matching activity added.

---
+++ app/src/main/java/com/company/tookar/ui/activity/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.company.tookar.ui.activity;
+
+import android.os.Bundle;
+
+import com.company.tookar.R;
+
+public class HomeActivity extends BaseActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
--- app/src/main/java/com/company/tookar/ui/activity/LoginActivity.java
+++ app/src/main/java/com/company/tookar/ui/activity/LoginActivity.java
@@ -1,8 +1,11 @@
 package com.company.tookar.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.company.tookar.R;
+
 
 public class LoginActivity extends BaseActivity
 {
@@ -11,6 +14,11 @@ public class LoginActivity extends BaseActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void prototypeAnonLogin(View view)
+    {
+        startActivity(new Intent(LoginActivity.this, HomeActivity.class));
     }
 }
