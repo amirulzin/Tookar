270d66ae16756aafcd73e5a2c82605e2a04f2921
Amirul Zin <amirul.mdzin@gmail.com>
2015-08-11 18:18:17 +0800

    Added more prototype layouts

--- app/src/main/java/com/company/tookar/ui/activity/HomeActivity.java
+++ app/src/main/java/com/company/tookar/ui/activity/HomeActivity.java
@@ -1,6 +1,10 @@
 package com.company.tookar.ui.activity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.company.tookar.R;
 
@@ -11,5 +15,30 @@ public class HomeActivity extends BaseActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        setupViews();
+    }
+
+    private void setupViews()
+    {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.base_toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setTitleTextColor(Color.WHITE);
+
+        findViewById(R.id.ah_goodwillcard).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+            }
+        });
+
+        findViewById(R.id.ah_matchcard).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(new Intent(HomeActivity.this, MatchingActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/company/tookar/ui/activity/MatchingActivity.java
+++ app/src/main/java/com/company/tookar/ui/activity/MatchingActivity.java
@@ -1,6 +1,8 @@
 package com.company.tookar.ui.activity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.company.tookar.R;
 
@@ -12,5 +14,14 @@ public class MatchingActivity extends BaseActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_matching);
+        setupViews();
+    }
+
+    private void setupViews()
+    {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.base_toolbar);
+        toolbar.setTitleTextColor(Color.WHITE);
+        toolbar.setTitle("Matched barters");
+        toolbar.setSubtitle("Local");
     }
 }
